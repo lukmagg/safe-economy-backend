5914d4a13114549ccfded247c9c28cadeca8e67d
Lucas Antonio Maggio <marsinformatica@gmail.com>
2024-06-13 20:51:54 +0200

    fix build

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Configuración de CORS para aceptar peticiones desde cualquier origen
-  app.enableCors({
-    origin: '*', // Permite cualquier origen
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
 
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(3001);
 }
 bootstrap();
